99464c7da59317f1c85d8245df718c5a66c9bdb6
Pratik Chaudhari <36737722+Proxxc29@users.noreply.github.com>
2020-04-26 12:28:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -494,7 +494,7 @@ def getValue():
 
 	
 if __name__ == '__main__':
-	app.run(host ='0.0.0.0', port = 5001, debug = True)
+	app.run(host ='0.0.0.0', port = 80, debug = True)
         #getValue()
 	
 	
